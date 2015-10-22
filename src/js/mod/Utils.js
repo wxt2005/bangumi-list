12980a27b19b456e23fb952fe45c1a173c04935d
fancl20 <fancl20@gmail.com>
2015-10-22 18:18:16 +0800

    简化代码

--- src/js/mod/Utils.js
+++ src/js/mod/Utils.js
@@ -139,8 +139,7 @@ function hasOnair(dateStr, timeStr){
 function hasEnded(dateStr, timeStr, offset){
     var now = new Date(),
         endDate = new Date(dateStr.replace(/-/g, '/') + ' ' +
-            timeStr.slice(0, 2) + ':' +
-            (+timeStr.slice(2)) +
+            timeStr.slice(0, 2) + ':' + timeStr.slice(2) +
             ' GMT+0800 (CST)');
     endDate.setDate(endDate.getDate() + (offset || 0))
     return now >= endDate;
