43b62f6c3e988a3cefabd6f98f364a285f55d866
Botao <wxt2005@gmail.com>
2018-08-07 22:43:04 +0800

    Add ad

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,5 @@ var MainApp = require('./component/MainApp');
 require('../less/main.less');
 
 MainApp.init();
+
+console.log('阿里巴巴旗下菜鸟网络（https://www.cainiao.com）招聘前端工程师，工作地杭州，简历请发至 wxt2005@gmail.com')
